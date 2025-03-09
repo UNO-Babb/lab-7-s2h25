407d065abaf15dd456b00b032019bf704bf9cb54
s2h25 <keithenvalentine@unomaha.edu>
2025-03-09 00:56:57 +0000

    Problem 7 done

--- Problem7.py
+++ Problem7.py
@@ -2,8 +2,24 @@
 #Project Euler problem 7
 
 from NumberTests import isPrime
+
+def nth_prime(p):
+    """Return the nth prime number"""
+    count = 0
+    num = 1
+
+    while count < p:
+        num += 1
+        if isPrime(num):
+           count += 1
+    
+    return num
+
 def main():
+    p = 10001
+    result = nth_prime(p)
+    print(f"The {p}th prime number is: {result}")
   
 
-if __name__ == '__main__':
-  main()
+if __name__ == '__main__': 
+    main()
